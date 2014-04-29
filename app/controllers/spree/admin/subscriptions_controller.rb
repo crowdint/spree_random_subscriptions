c95a6013cfe1dc36e2e94621774672a67f7166b3
Mumo Carlos <mumo.crls@gmail.com>
2014-04-29 15:32:56 -0500

    [#70017272] Admin subscriptions manager
    * Adds cancel ability to admins
    * Sorts en translations

--- app/controllers/spree/admin/subscriptions_controller.rb
+++ app/controllers/spree/admin/subscriptions_controller.rb
@@ -1,8 +1,20 @@
 module Spree
   module Admin
     class SubscriptionsController < Spree::Admin::BaseController
+      before_action :load_subscription, only: :destroy
+
       def index
         @subscriptions = Spree::Subscription.all
+      end
+
+      def destroy
+        @subscription.cancel
+        redirect_to spree.admin_subscriptions_path
+      end
+
+      private
+      def load_subscription
+        @subscription ||= Spree::Subscription.find_by_id params[:id]
       end
     end
   end
