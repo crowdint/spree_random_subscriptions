a411b92347cf0a594b2e15625eaf706b5ab6afc4
Mumo Carlos <mumo.crls@gmail.com>
2014-05-04 20:10:09 -0500

    Adds factories for easy load

--- lib/spree_random_subscriptions/factories.rb
+++ lib/spree_random_subscriptions/factories.rb
@@ -1,4 +1,36 @@
 FactoryGirl.define do
+  factory :subscription, class: Spree::Subscription do
+    before(:create) do
+      create(:product) if Spree::Product.unsubscribable.count == 0
+    end
+
+    user { create :user }
+    address { create :address }
+    subscription_product { create :subscription_product }
+    limit 12
+  end
+
+  factory :base_subscription_product, class: Spree::SubscriptionProduct do
+    sequence(:name) { |n| "Product ##{n} - #{Kernel.rand(9999)}" }
+    description { generate(:random_description) }
+    price 19.99
+    cost_price 17.00
+    sku 'ABC'
+    available_on { 1.year.ago }
+    deleted_at nil
+    shipping_category { |r| Spree::ShippingCategory.first || r.association(:shipping_category) }
+
+    # ensure stock item will be created for this products master
+    before(:create) { create(:stock_location) if Spree::StockLocation.count == 0 }
+
+    after(:create) do |p|
+      p.variants_including_master.each { |v| v.save! }
+    end
+
+    factory :subscription_product do
+      tax_category { |r| Spree::TaxCategory.first || r.association(:tax_category) }
+    end
+  end
   # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
   #
   # Example adding this to your spec_helper will load these Factories for use:
