1e78635f06a0d69eff0e1309ecaf1501562825bb
Mumo Carlos <mumo.crls@gmail.com>
2014-05-05 12:42:48 -0500

    [#68239646] One Shipping Per Subscription
    * Refactors orders decorator

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -2,27 +2,34 @@ module Spree
   OrderContents.class_eval do
 
     private
-
     def add_to_line_item(variant, quantity, currency=nil, shipment=nil)
-      line_item = grab_line_item_by_variant(variant)
+      @item_to_add = grab_line_item_by_variant(variant)
 
-      if line_item && !line_item.product_subscription?
-        line_item.target_shipment = shipment
-        line_item.quantity += quantity.to_i
-        line_item.currency = currency unless currency.nil?
+      if @item_to_add && !@item_to_add.product_subscription?
+        update_existing_line_item(quantity, shipment, currency)
       else
-        line_item = order.line_items.new(quantity: quantity, variant: variant)
-        line_item.target_shipment = shipment
-        if currency
-          line_item.currency = currency
-          line_item.price    = variant.price_in(currency).amount
-        else
-          line_item.price    = variant.price
-        end
+        add_new_line_item(variant, quantity, currency, shipment)
       end
 
-      line_item.save
-      line_item
+      @item_to_add.save
+      @item_to_add
+    end
+
+    def add_new_line_item(variant, quantity, currency, shipment)
+      @item_to_add = order.line_items.new(quantity: quantity, variant: variant)
+      @item_to_add.target_shipment = shipment
+      if currency
+        @item_to_add.currency = currency
+        @item_to_add.price    = variant.price_in(currency).amount
+      else
+        @item_to_add.price    = variant.price
+      end
+    end
+
+    def update_existing_line_item(quantity, shipment, currency)
+      @item_to_add.target_shipment = shipment
+      @item_to_add.quantity += quantity.to_i
+      @item_to_add.currency = currency if currency
     end
 
   end
