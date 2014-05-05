5cfd9a1191a1555b299986257766e61811764a3b
Mumo Carlos <mumo.crls@gmail.com>
2014-05-04 20:05:49 -0500

    [#68239646] One Shipping Address Per Subscription
    * Add ship_address to line item

---
+++ app/models/spree/line_item_decorator.rb
@@ -0,0 +1,7 @@
+module Spree
+  LineItem.class_eval do
+    belongs_to :ship_address, foreign_key: :ship_address_id, class_name: 'Spree::Address'
+    alias_attribute :shipping_address, :ship_address
+  end
+end
+
