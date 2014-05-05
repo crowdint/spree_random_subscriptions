f461a37f2e213ccd62afe11343a87bf7ec701bcb
Mumo Carlos <mumo.crls@gmail.com>
2014-05-05 10:21:21 -0500

    [#68239646] One Shipping Address Per Subscription
    * Deletes unneded migration
    * Adds migration for adding line item id to spree addresses

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   LineItem.class_eval do
-    belongs_to :ship_address, foreign_key: :ship_address_id, class_name: 'Spree::Address'
+    has_one :ship_address, class_name: 'Spree::Address'
     alias_attribute :shipping_address, :ship_address
 
     delegate :subscription?, to: :product, prefix: true
--- db/migrate/20140504235734_add_ship_address_id_to_spree_line_items.rb
+++
@@ -1,5 +0,0 @@
-class AddShipAddressIdToSpreeLineItems < ActiveRecord::Migration
-  def change
-    add_column :spree_line_items, :ship_address_id, :integer, index: true
-  end
-end
