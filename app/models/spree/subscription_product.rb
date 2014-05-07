99bf70cffc8be0f70977df831b77e82cee4a8af1
Steven Barragan <me@steven.mx>
2014-05-07 13:58:43 -0500

    Generate a subscription product and calculate its price

--- app/models/spree/subscription_product.rb
+++ app/models/spree/subscription_product.rb
@@ -1,4 +1,64 @@
 module Spree
   class SubscriptionProduct < Product
+    attr_accessor :wrap_cost
+    attr_accessor :wrap_type
+
+    def self.generate(gender, recurring, wrap_type, limit = nil, price = 11, wrap_cost = 2)
+      product = new(
+        recurring: recurring,
+        wrap_cost: wrap_cost,
+        name: '',
+        description: '',
+        limit: limit,
+        shipping_category_id: 1
+      )
+
+      product.gender = gender
+      product.original_price = price
+      product.wrap = wrap_type
+      product.recurring_limit = limit
+
+      product.calculate_price
+      product.save
+      product
+    end
+
+    def original_price=(price)
+      self.price = price
+      self.description += "- Price: $#{price}\n"
+    end
+
+    def gender=(gender)
+      self.name += "Socks for #{gender}"
+      self.description += "- For #{ gender }\n"
+    end
+
+    def wrap=(wrap)
+      unless wrap == 'none'
+        self.name += " - Wrap #{ wrap }"
+
+        if wrap == 'first month'
+          self.description += "- Wrap #{ wrap } $#{ wrap_cost}\n"
+        else
+          self.description += "- Wrap #{ wrap } $#{ wrap_cost } X #{ limit } = $#{ wrap_cost * limit }\n"
+        end
+      end
+
+      @wrap_type = wrap
+    end
+
+    def recurring_limit=(limit)
+      self.limit = limit
+      self.name += " - By #{ limit } months" if limit
+    end
+
+    def calculate_price
+      if recurring
+        self.price += @wrap_cost if @wrap_type == 'every month'
+        self.price *= limit
+      end
+
+      self.price += @wrap_cost if @wrap_type == 'first month'
+    end
   end
 end
---
+++ spec/models/spree/subscription_product_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Spree::SubscriptionProduct do
+  describe '.generate' do
+    context 'man, no recurring, first month wrapping' do
+      let(:subject) { described_class.generate('man', false, 'first month') }
+
+      it { expect(subject.name).to eq 'Socks for man - Wrap first month' }
+
+      it { expect(subject.description).to eq "- For man\n- Price: $11\n- Wrap first month $2\n" }
+
+      it { expect(subject.price.to_f).to eq 13 }
+    end
+
+    context 'woman, recurring, first month wrapping, 12 months' do
+      let(:subject) { described_class.generate('woman', true, 'first month', 12) }
+
+      it { expect(subject.name).to eq 'Socks for woman - Wrap first month - By 12 months' }
+
+      it { expect(subject.description).to eq "- For woman\n- Price: $11\n- Wrap first month $2\n" }
+
+      it { expect(subject.price.to_f).to eq 134 }
+    end
+
+    context 'man, recurring, each month wrapping, 6 months' do
+      let(:subject) { described_class.generate('man', true, 'every month', 6) }
+
+      it { expect(subject.name).to eq 'Socks for man - Wrap every month - By 6 months' }
+
+      it { expect(subject.description).to eq "- For man\n- Price: $11\n- Wrap every month $2 X 6 = $12\n" }
+
+      it { expect(subject.price.to_f).to eq 78 }
+    end
+  end
+end
