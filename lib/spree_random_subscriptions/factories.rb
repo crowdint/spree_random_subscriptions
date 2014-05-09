9b6a7a397714d4c29157f246ca2f6a9458c2c676
Mumo Carlos <mumo.crls@gmail.com>
2014-05-09 17:31:03 -0500

    Require spec factories on lib/spree_random_subscriptions/factories.rb

--- lib/spree_random_subscriptions/factories.rb
+++ lib/spree_random_subscriptions/factories.rb
@@ -1,6 +1,1 @@
-FactoryGirl.define do
-  # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
-  #
-  # Example adding this to your spec_helper will load these Factories for use:
-  # require 'spree_random_subscriptions/factories'
-end
+Dir[File.join(File.dirname(__FILE__), '../../spec/factories/**/*.rb')].each { |f| require f }
