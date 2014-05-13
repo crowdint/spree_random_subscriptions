3b81c4f16e12e5fa100994bc648334acc2492155
Juan Carlos <zalapeach@gmail.com>
2014-05-13 09:39:43 -0500

    [70964900] Adding specs for multiple subscription product
    * (WIP) Added more capybara specs

--- spec/features/users/subscriptions_spec.rb
+++ spec/features/users/subscriptions_spec.rb
@@ -4,11 +4,19 @@ include Warden::Test::Helpers
 feature 'shipping address for subscription' do
   let!(:address) { create(:address) }
   let!(:user) { create(:user, billing_address: address) }
+  let!(:shipping_category) do
+    create(:shipping_category, shipping_methods: [shipping_method])
+  end
+  let!(:shipping_method) { create(:shipping_method) }
   let!(:subscription_product1) do
-    create(:subscription_product, name: 'Subscription 1')
+    create(:subscription_product,
+           name: 'Subscription 1',
+           shipping_category: shipping_category)
   end
   let!(:subscription_product2) do
-    create(:subscription_product, name: 'Subscription 2')
+    create(:subscription_product,
+           name: 'Subscription 2',
+           shipping_category: shipping_category)
   end
   let!(:order) { create(:order, state: 'cart', user: user) }
   let!(:line_item1) do
@@ -43,12 +51,12 @@ feature 'shipping address for subscription' do
       end
     end
 
-    context 'checkout delivery page' do
+    context 'checkout delivery page', js: true do
       scenario 'checking the values for subscription shipment' do
         page.find(:css, '#order_use_billing').set(true)
         click_button 'Save and Continue'
-        expect(page).to have_content('Subscription product 1')
-        expect(page).to have_content('Subscription product 2')
+        expect(page).to have_content('Subscription 1')
+        expect(page).to have_content('Subscription 2')
         expect(page).to have_content(
           "#{ address.firstname } #{ address.lastname }: #{ address.address1 }"
         )
@@ -60,14 +68,16 @@ feature 'shipping address for subscription' do
         scenario 'changing shipping address for one subscription' do
           page.find(:css, '#order_use_billing').set(true)
           page.find(:css, '#1-address-checkbox').set(false)
-          binding.pry
+          #binding.pry
           within('#1-address') do
             fill_in 'First Name', with: 'Jose'
             fill_in 'Last Name', with: 'Cuervo'
             fill_in 'Street Address', with: 'Something there #332'
+            #binding.pry
             fill_in 'City', with: 'Springfield'
             select 'United States of America', from: 'Country'
-            select 'Texas', from: 'State'
+            fill_in 'State', with: 'Texas'
+            select 'Alabama', from: 'State'
             fill_in 'Zip', with: '28000'
             fill_in 'Phone', with: '3121111111'
           end
