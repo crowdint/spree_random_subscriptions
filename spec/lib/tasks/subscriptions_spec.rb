4c6af725421215fb76ccda4b75626273a1c9cef8
Jesus Figueroa <jesus.figueroa@crowdint.com>
2014-07-02 17:10:01 -0500

    Removed unnecessary test

--- spec/lib/tasks/subscriptions_spec.rb
+++ spec/lib/tasks/subscriptions_spec.rb
@@ -21,30 +21,4 @@ describe 'subscriptions', sidekiq: :fake do
       end.to change(Sidekiq::Extensions::DelayedClass.jobs, :size).by(1)
     end
   end
-
-  describe ':generate_products' do
-    it do
-      expect do
-        Rake::Task['subscriptions:generate_products'].invoke
-      end.to change(Sidekiq::Extensions::DelayedClass.jobs, :size).by(60)
-    end
-
-    context 'generate all subscription products combinations' do
-      before do
-        Rake::Task['subscriptions:generate_products'].invoke
-      end
-
-      it 'generates man no recurring first month wrapping product' do
-        expect do 
-          Spree::SubscriptionProduct.find_by(name: 'Socks for man - Pay once - Wrap first month - By 3 months')
-        end.to be_true
-      end
-
-      it 'man, no recurring, each month wrapping, 6 months' do
-        expect do 
-          Spree::SubscriptionProduct.find_by(name: 'Socks for man - Pay once - Wrap every month - By 6 months')
-        end.to be_true
-      end
-    end
-  end
 end
